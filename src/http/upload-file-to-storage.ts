57140e73e641bd6c82c15fa5cde7e8cc97d05fc3
sillas.braga <sillas.braga@sankhya.com.br>
2025-12-31 14:11:59 -0300

    commit

---
+++ src/http/upload-file-to-storage.ts
@@ -0,0 +1,32 @@
+import axios from "axios";
+
+interface UploadFileToStorageParams {
+  file: File;
+  onProgress: (sizeInBytes: number) => void
+}
+
+interface UploadFileToStorageOptions {
+  signal?: AbortSignal;
+}
+
+export async function uploadFileToStorage({ file, onProgress }: UploadFileToStorageParams, opts?: UploadFileToStorageOptions) {
+  const data = new FormData();
+
+  data.append("file", file);
+
+  const response = await axios.post<{ url: string }>(
+    "http://localhost:3333/uploads",
+    data,
+    {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+      signal: opts?.signal,
+      onUploadProgress(processEvent) {
+         onProgress(processEvent.loaded)
+      }
+    }
+  );
+
+  return { url: response.data.url };
+}
